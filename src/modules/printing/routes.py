5a0781e20ef9e78f505a743022a968f01c481b55
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2025-05-15 04:34:29 +0300

    fix: pass cups_name

--- src/modules/printing/routes.py
+++ src/modules/printing/routes.py
@@ -54,7 +54,7 @@ def get_printers(_innohassle_user_id: USER_AUTH) -> list[Printer]:
 def get_printers_status(_innohassle_user_id: USER_AUTH) -> list[PrinterStatus]:
     result: list[PrinterStatus] = []
     for printer in settings.api.printers_list:
-        status = printing_repository.get_printer_status(printer.name)
+        status = printing_repository.get_printer_status(printer.cups_name)
         logger.info(f"Printer {printer.name} status: {status}")
         result.append(
             PrinterStatus(
