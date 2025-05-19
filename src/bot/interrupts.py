4ec046072df40f7f75d48207447b009b27778855
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2025-05-19 13:30:21 +0300

    chore: ignore exceptions during graceful interrupt

--- src/bot/interrupts.py
+++ src/bot/interrupts.py
@@ -1,7 +1,10 @@
+import httpx
 from aiogram import Bot
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 
+from src.api.logging_ import logger
 from src.bot.routers.printing.printing_states import gracefully_interrupt_printing_state
 from src.bot.routers.scanning.scanning_states import gracefully_interrupt_scanning_state
 
@@ -9,5 +12,8 @@ UNINTERRUPTIBLE_STATES = []
 
 
 async def gracefully_interrupt_state(callback_or_message: CallbackQuery | Message, state: FSMContext, bot: Bot):
-    await gracefully_interrupt_printing_state(callback_or_message, state, bot)
-    await gracefully_interrupt_scanning_state(callback_or_message, state, bot)
+    try:
+        await gracefully_interrupt_printing_state(callback_or_message, state, bot)
+        await gracefully_interrupt_scanning_state(callback_or_message, state, bot)
+    except (TelegramBadRequest, httpx.HTTPError) as e:
+        logger.error(f"Error gracefully interrupting state: {e}")
